6f3b49f5baecd270dfcbeb170e25da62a735eb30
Abdellah <abdallahkaba98@gmail.com>
2024-04-15 22:38:11 +0000

    BarMenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuItem, MatMenuModule, MatMenuTrigger} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatMenuTrigger,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
