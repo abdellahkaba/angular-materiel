0c99b4bc495002195085d7086f1e9a879691972c
Abdellah <abdallahkaba98@gmail.com>
2024-04-16 11:26:00 +0000

    logout

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  public username! : string
+  public username! : any
   public roles : any
-  constructor() { }
+  public authenticated : boolean = false
+  constructor(private router : Router) { }
 
   public login(username: string,password : string) {
     if(username == "admin" && password=="password"){
       this.username = username
       this.roles = ['ADMIN']
+      this.authenticated = true
       return true
     }else {
       return false
     }
+  }
+
+  logout() {
+    this.authenticated = false
+    this.username = undefined
+    this.roles = undefined
+    this.router.navigateByUrl("/login")
   }
 }
